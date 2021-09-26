e130b11c34bc9d5d36b76421a90f4a54fcc36d1b
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-09-26 12:10:15 +0300

    added price

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,10 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+const products = document.querySelectorAll('.product');
+products.forEach(product => {
+  let price = document.querySelector('.price').textContent;
+  product.setAttribute("data-price", price);
+})
 
 });
